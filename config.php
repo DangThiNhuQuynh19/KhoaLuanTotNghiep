a914e563dcc12e5a6dd79b194daae52dcc6025d9
nguyentrang2642003@gmail.com <nguyentrang2642003@gmail.com>
2025-11-15 23:17:55 +0700

    fix logingoogle

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once 'vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId('464279125155-dshf51l06kgncpfe3vqn6k3v9uvel5jq.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('464279125155-1nghqp2efppp05jqhphjsasa2e2t7jsg.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri('https://hanhphuc.site/?action=logingoogle');
 $client->addScope("email");
 $client->addScope("profile");
