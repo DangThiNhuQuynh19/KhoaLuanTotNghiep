39be05cf367e521066eb28bab5c74f0a3c8688b3
nguyentrang2642003@gmail.com <nguyentrang2642003@gmail.com>
2025-11-15 23:26:21 +0700

    google

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once 'vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId('464279125155-1nghqp2efppp05jqhphjsasa2e2t7jsg.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('562721365946-o3phmrsbmp9tvnp3gb03i7q0pr2o4cg4.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri('https://hanhphuc.site/?action=logingoogle');
 $client->addScope("email");
 $client->addScope("profile");
