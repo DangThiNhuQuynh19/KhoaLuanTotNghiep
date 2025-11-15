279c6990656f27d5b0469de71b791f2e9a804ac3
nguyentrang2642003@gmail.com <nguyentrang2642003@gmail.com>
2025-11-15 23:13:17 +0700

    db

--- Models/ketnoi.php
+++ Models/ketnoi.php
@@ -1,7 +1,7 @@
 <?php
 class clsketnoi{
     public function moKetNoi(){
-        $con = mysqli_connect("160.30.136.31", "root", "kltntrangquynh2025@", "hanhphuc");
+        $con = mysqli_connect("localhost", "root", "kltntrangquynh2025@", "hanhphuc");
         mysqli_set_charset($con,'utf8');
         return $con;
     }
