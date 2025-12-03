1be65be96ef917599e9c5ff7d15f3b7ab2df0299
NguyenThanhThuyTrang <nguyentrang2642003@gmail.com>
2025-12-03 07:42:29 +0000

    Remove config.php from tracking

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-
-$client = new Google_Client();
-$client->setClientId('562721365946-fnu3fn3uultptrqneeg01p890c5ajnqf.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('https://hanhphuc.site/?action=logingoogle');
-$client->addScope("email");
-$client->addScope("profile");
