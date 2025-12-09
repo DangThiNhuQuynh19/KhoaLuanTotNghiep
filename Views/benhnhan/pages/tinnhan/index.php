f05c52117df4f2dd09e0b6ab0cc75668499071ab
nguyentrang2642003@gmail.com <nguyentrang2642003@gmail.com>
2025-12-10 05:53:55 +0700

    git push

--- Views/benhnhan/pages/tinnhan/index.php
+++ Views/benhnhan/pages/tinnhan/index.php
@@ -21,7 +21,6 @@ $tentk = $_SESSION['user']['tentk'];
         * { box-sizing: border-box; margin: 0; padding: 0; }
         html, body { height: 100%; }
         body {
-            padding-top: 100px;
             font-family: 'Poppins', sans-serif;
             background: #f0f2f5;
             color: #0f1724;
@@ -38,6 +37,7 @@ $tentk = $_SESSION['user']['tentk'];
             background: white;
             display: flex;
             margin-top: calc(var(--site-header-height) + var(--chat-gutter));
+            margin-top: 100px;
             margin-bottom: calc(var(--site-footer-height) + var(--chat-gutter));
         }
 
